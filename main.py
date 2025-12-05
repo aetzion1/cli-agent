42ed956b814f86cf6a0260ae7f6472d0d9350c67
Adam Etzion <adametzion90@gmail.com>
2025-12-05 14:54:26 -0700

    Add verbose flag

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from google.genai import types
 def main():
     parser = argparse.ArgumentParser(description="AI Code Assistant")
     parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     load_dotenv()
@@ -25,8 +26,10 @@ def main():
     if not response.usage_metadata:
         raise RuntimeError("Failed API request, no usage metadata produced")
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
 
